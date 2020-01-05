043cc02fb430dd639c9d5cd862a4bc6112ebbf16
Ramin Nabati <mnabati@vols.utk.edu>
2020-01-05 00:23:14 -0500

    Minor changes to setup.py and requirements.txt

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mrnabati/nuscenes_dataset",
-    packages=setuptools.find_packages(exclude=['tests', '__pycache__', '*.__pycache__', '__pycache.*', '*.__pycache__.*']),
+    packages=setuptools.find_packages("pynuscenes", exclude=['__pycache__', '*.__pycache__', '__pycache.*', '*.__pycache__.*']),
+    package_dir={'':'pynuscenes'},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
